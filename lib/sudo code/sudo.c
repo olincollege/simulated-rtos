e39c83579099f1a5f56be947c817b565ce22d57d
cleira0501 <ariel920501@gmail.com>
2025-04-07 11:58:59 -0400

    hello world test code

---
+++ lib/sudo code/sudo.c
@@ -0,0 +1,14 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+// define max task we can take
+
+// create task control block stuct
+// include task statuses(ready, running, blocked, done)
+// define
+
+// scheduler
+
+// context switching
+// create task
