433d6e7f1c74a099c992cf46f43ed6149b934562
zcoakley <zara.samemail@gmail.com>
2025-04-11 13:26:05 -0400

    Add cross-compilation files (not working)

--- cmake/arm-gcc-toolchain.cmake
+++ cmake/arm-gcc-toolchain.cmake
@@ -1,10 +1,23 @@
-# arm-gcc-toolchain.cmake
+# Specify that this is a bare-metal embedded system
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(TOOLCHAIN_PREFIX arm-none-eabi-)
+# Set the compilers
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 
-set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
-set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}gcc)
-set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
-set(CMAKE_SIZE ${TOOLCHAIN_PREFIX}size)
+# Optional: set other toolchain utilities
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+set(CMAKE_AR arm-none-eabi-ar)
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+set(CMAKE_SIZE arm-none-eabi-size)
+set(CMAKE_LINKER arm-none-eabi-ld)
+
+# Prevent CMake from trying to use host libraries or includes
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# Avoid toolchain test programs requiring a working OS
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
--- src/main.c
+++ src/main.c
@@ -1,14 +1,78 @@
-// import everything
-//  Define tasks 1, 2, and 3
+// #include <stdio.h>
 
-//  main()
-//  Make task queue
-//  Add tasks 1,2,3 to task queue
-//  Run the scheduler
+// int main(void) {
+//   // Clock setup -- idk if we need this
+//   // rcc_clock_setup_hse_3v3(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
 
+//   printf("Hello, world!\n");
+//   return 0;
+// }
+
+#include <errno.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
 #include <stdio.h>
+#include <unistd.h>
+
+static void clock_setup(void) {
+  /* Enable GPIOD clock for LED & USARTs. */
+  rcc_periph_clock_enable(RCC_GPIOD);
+  rcc_periph_clock_enable(RCC_GPIOA);
+
+  /* Enable clocks for USART2. */
+  rcc_periph_clock_enable(RCC_USART2);
+}
+
+static void usart_setup(void) {
+  /* Setup USART2 parameters. */
+  usart_set_baudrate(USART2, 115200);
+  usart_set_databits(USART2, 8);
+  usart_set_stopbits(USART2, USART_STOPBITS_1);
+  usart_set_mode(USART2, USART_MODE_TX);
+  usart_set_parity(USART2, USART_PARITY_NONE);
+  usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+
+  /* Finally enable the USART. */
+  usart_enable(USART2);
+}
+
+static void gpio_setup(void) {
+  /* Setup GPIO pin GPIO12 on GPIO port D for LED. */
+  gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+
+  /* Setup GPIO pins for USART2 transmit. */
+  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+
+  /* Setup USART2 TX pin as alternate function. */
+  gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+}
+
+int _write(int file, char *ptr, int len) {
+  int i;
+
+  if (file == STDOUT_FILENO || file == STDERR_FILENO) {
+    for (i = 0; i < len; i++) {
+      if (ptr[i] == '\n') {
+        usart_send_blocking(USART2, '\r');
+      }
+      usart_send_blocking(USART2, ptr[i]);
+    }
+    return i;
+  }
+  errno = EIO;
+  return -1;
+}
 
 int main(void) {
-  printf("Hello, world!\n");
+  clock_setup();
+  gpio_setup();
+  usart_setup();
+
+  printf("hello world!\n");
+
+  while (1) {
+  }
+
   return 0;
 }
