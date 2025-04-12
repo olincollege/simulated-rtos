59ea05f88fe34dfa063500cd6c3184bd3f4d9556
zcoakley <zara.samemail@gmail.com>
2025-04-12 16:05:57 -0400

    Add tutorial hello world setup

---
+++ Makefile_renode_example.mk
@@ -0,0 +1,14 @@
+PROJECT = renode-example
+BUILD_DIR = bin
+
+CFILES = renode-example.c lib/my_lib.c
+INCLUDES += -Ilib
+
+DEVICE=stm32f407vgt6
+OOCD_FILE = board/stm32f4discovery.cfg
+
+OPENCM3_DIR=libopencm3
+
+include $(OPENCM3_DIR)/mk/genlink-config.mk
+include rules.mk
+include $(OPENCM3_DIR)/mk/genlink-rules.mk
