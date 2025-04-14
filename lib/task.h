2bccc928a200eb828155be2fb6c31e08dc7c225d
cleira0501 <ariel920501@gmail.com>
2025-04-14 11:56:54 -0400

    moved tcb

--- lib/task.h
+++ lib/task.h
@@ -1,3 +1,13 @@
+typedef void (*Task_func)();
+// a pointer to a function that takes in nothing and returns void
+typedef struct {
+  Task_func func;
+  int priority;
+  int is_active;
+  // task ID
+
+} TaskControlBlock;
+
 void task_1(void);
 void task_2(void);
 void task_3(void);
