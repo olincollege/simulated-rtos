a419f129f835fcf445091934eefd025e5c3268a9
cleira0501 <ariel920501@gmail.com>
2025-04-14 20:29:40 -0400

    finished mvp for queue

--- lib/queue.c
+++ lib/queue.c
@@ -4,32 +4,21 @@
 
 #include "task.h"
 
-void enqueue(Queue* q, QueueNode* new_task) {};
+void enqueue(Queue* q, QueueNode* new_task) {
+  if (q->length == 0) {
+    q->front = new_task;
+    q->last = new_task;
+    q->length = 1;
+    return;
+  }
+  q->last->next = new_task;
+};
 
-QueueNode* pop(Queue* q) { return NULL; };
-
-// int queue_size = 0;
-// // return queue length
-// // pop the highest priority
-// // insert task to queue
-// //
-
-// /* Task Queue (Priority queue, add task and sort, remove no need to sort) */
-// static struct TaskControlBlock gs_task_queue[MAX_NUMBER_OF_TASKS];
-// /* Task queue length */
-// static uint32_t task_queue_len;
-// /* Id for the task (must be unique for all the active tasks) */
-// static uint32_t next_task_id;
-
-// // initialize task queue
-// void init_task_queue() {
-//   /* Reset the queue index */
-//   task_queue_len = 0;
-
-//   /* Reset the task id */
-//   next_task_id = 0;
-// }
-
-// int queue_is_empty(void) { return queue_size == 0; }
-// int insert_task(int task_id, int priority_num) {}
-// struct TaskControlBlock* pop_task  // returns task ID?
+QueueNode* pop(Queue* q) {
+  if (q->length == 0) {
+    perror("queue is empty");
+    return;
+  }
+  q->length -= 1;
+  return q->front;
+};
