be68d2e59b681d58879032b41687a42bbedabf3e
zcoakley <zara.samemail@gmail.com>
2025-04-16 12:26:49 -0400

    Add newline char on task printouts

--- lib/task.c
+++ lib/task.c
@@ -5,6 +5,6 @@
 /*
 simulate tasks here
 */
-void task_1(void) { printf("task_1 is running"); }
-void task_2(void) { printf("task_2 is running"); }
-void task_3(void) { printf("task_3 is running"); }
+void task_1(void) { printf("task_1 is running\n"); }
+void task_2(void) { printf("task_2 is running\n"); }
+void task_3(void) { printf("task_3 is running\n"); }
