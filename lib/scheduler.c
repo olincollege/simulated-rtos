c9559daddc53489217ede735d08ff016de7f9b84
zcoakley <zara.samemail@gmail.com>
2025-04-22 21:21:41 -0400

    Start implementing scheduler timer capabilities

--- lib/scheduler.c
+++ lib/scheduler.c
@@ -3,10 +3,16 @@
 #include "queue.h"
 #include "task.h"
 
-int task_count = 0;
-
 void run_scheduler(Queue* queue) {
   while (1) {
+    // Schedule an interrupt after 2 ms of running the task
+    /* Set the initual output compare value for OC1. */
+    timer_set_oc_value(TIM2, TIM_OC1, TWO_MILLISECONDS);
+    /* Counter enable. */
+    timer_enable_counter(TIM2);
+    /* Enable Channel 1 compare interrupt.*/
+    timer_enable_irq(TIM2, TIM_DIER_CC1IE);
+
     // pop task node from queue
     QueueNode* node = pop(queue);
     TaskControlBlock* tcb = node->tcb;
