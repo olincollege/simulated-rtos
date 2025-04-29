76a72fe0f31a780e5fff1e57ff061e9510810784
zcoakley <zara.samemail@gmail.com>
2025-04-28 20:27:48 -0400

    Clean up task.h

--- lib/task.h
+++ lib/task.h
@@ -1,9 +1,14 @@
 #pragma once
 #include <stdbool.h>
+
 struct TaskControlBlock;
-#define MAX_SHORT_TASK 15
-typedef void (*TaskFunc)(struct TaskControlBlock*);
+
+// Set the maximum number of short tasks, whose nodes will be preallocated
+const size_t MAX_SHORT_TASK = 15;
+
 // a pointer to a function that takes in nothing and returns void
+typedef void (*TaskFunc)(struct TaskControlBlock*);
+
 typedef enum { WARNING_PRIORITY = 1, REGULAR_PRIORITY = 3 } TaskPriority;
 
 typedef struct TaskControlBlock {
@@ -17,4 +22,3 @@ typedef struct TaskControlBlock {
 
 void long_task(TaskControlBlock* long_task_tcb);
 void short_task(TaskControlBlock* short_task_tcb);
-// void task_3(void);
