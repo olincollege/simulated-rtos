a932e49c4d65161a1b06b81728ad4c45c1c4b1df
zcoakley <zara.samemail@gmail.com>
2025-04-29 21:56:19 -0400

    Add scheduler docstring

--- lib/scheduler.h
+++ lib/scheduler.h
@@ -8,7 +8,14 @@
 // With the timer running at 5kHz, this many ticks equals a 2 ms delay
 extern const int TWO_MILLISECONDS;
 
-/*
-Run the scheduler function. Runs indefinitely.
-*/
+/**
+ * @brief Run the scheduler function.
+ *
+ * The scheduler runs indefinitely. It pops a task from the task queue,
+ * then calls the function. It also schedules a TIM2 interrupt so that
+ * any task it calls can only run for a maximum of 2 ms = 10 TIM2 timer
+ * ticks. After a task finishes, it is requeued if it is a long_task.
+ *
+ * @param queue (Queue*) The task queue (already populated).
+ */
 void run_scheduler(Queue*);
