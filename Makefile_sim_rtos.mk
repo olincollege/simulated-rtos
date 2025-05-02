14edd1db82a41c4bae9377562a1767fc0671b7e6
zcoakley <zara.samemail@gmail.com>
2025-05-02 14:43:40 -0400

    Remove unnecessary line

--- Makefile_sim_rtos.mk
+++ Makefile_sim_rtos.mk
@@ -1,14 +1,8 @@
 PROJECT = simulated-rtos
 BUILD_DIR = bin
 
-CFILES := sim_rtos.c $(wildcard lib/*.c)#changed
+CFILES := sim_rtos.c $(wildcard lib/*.c)
 INCLUDES += -Ilib
-
-OBJS := $(CFILES:.c=.o)#added
-DEPS := $(OBJS:.o=.d)#added
-
-
-CFLAGS += -Wall -Wextra -pedantic -Os -Ilib -MD -MP#added
 
 DEVICE=stm32f407vgt6
 OOCD_FILE = board/stm32f4discovery.cfg
@@ -18,8 +12,4 @@ OPENCM3_DIR=libopencm3
 include $(OPENCM3_DIR)/mk/genlink-config.mk
 include rules.mk
 include $(OPENCM3_DIR)/mk/genlink-rules.mk
-#added below
-.PHONY: clean
-clean:
-	rm -f $(OBJS) $(DEPS) $(PROJECT).elf $(PROJECT).bin $(PROJECT).hex
 
